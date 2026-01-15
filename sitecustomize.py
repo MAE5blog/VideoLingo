175d67d94f69cb295a03be1a0b41ea898dbe9242
MAE5blog <admin@mae5.com>
2026-01-15 17:54:09 +0800

    Stub torchaudio AudioMetaData for WhisperX

--- sitecustomize.py
+++ sitecustomize.py
@@ -28,6 +28,18 @@ try:
         common_mod.set_audio_backend = torchaudio.set_audio_backend
         common_mod.get_audio_backend = torchaudio.get_audio_backend
         common_mod.list_audio_backends = torchaudio.list_audio_backends
+        class AudioMetaData:
+            def __init__(self, sample_rate=None, num_frames=None, num_channels=None, bits_per_sample=None, encoding=None):
+                self.sample_rate = sample_rate
+                self.num_frames = num_frames
+                self.num_channels = num_channels
+                self.bits_per_sample = bits_per_sample
+                self.encoding = encoding
+
+            def __iter__(self):
+                return iter((self.sample_rate, self.num_frames, self.num_channels, self.bits_per_sample, self.encoding))
+
+        common_mod.AudioMetaData = AudioMetaData
         sys.modules["torchaudio.backend.common"] = common_mod
 except Exception:
     pass
