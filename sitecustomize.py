c533f8720a34cbf918a981912483edb085e15524
MAE5blog <admin@mae5.com>
2026-01-15 19:57:04 +0800

    Ensure torchaudio backend common stubs always updated

--- sitecustomize.py
+++ sitecustomize.py
@@ -23,24 +23,25 @@ try:
     backend_mod.get_audio_backend = torchaudio.get_audio_backend
     backend_mod.list_audio_backends = torchaudio.list_audio_backends
 
-    if "torchaudio.backend.common" not in sys.modules:
+    common_mod = sys.modules.get("torchaudio.backend.common")
+    if common_mod is None:
         common_mod = types.ModuleType("torchaudio.backend.common")
-        common_mod.set_audio_backend = torchaudio.set_audio_backend
-        common_mod.get_audio_backend = torchaudio.get_audio_backend
-        common_mod.list_audio_backends = torchaudio.list_audio_backends
-        class AudioMetaData:
-            def __init__(self, sample_rate=None, num_frames=None, num_channels=None, bits_per_sample=None, encoding=None):
-                self.sample_rate = sample_rate
-                self.num_frames = num_frames
-                self.num_channels = num_channels
-                self.bits_per_sample = bits_per_sample
-                self.encoding = encoding
-
-            def __iter__(self):
-                return iter((self.sample_rate, self.num_frames, self.num_channels, self.bits_per_sample, self.encoding))
-
-        common_mod.AudioMetaData = AudioMetaData
         sys.modules["torchaudio.backend.common"] = common_mod
+    common_mod.set_audio_backend = torchaudio.set_audio_backend
+    common_mod.get_audio_backend = torchaudio.get_audio_backend
+    common_mod.list_audio_backends = torchaudio.list_audio_backends
+    class AudioMetaData:
+        def __init__(self, sample_rate=None, num_frames=None, num_channels=None, bits_per_sample=None, encoding=None):
+            self.sample_rate = sample_rate
+            self.num_frames = num_frames
+            self.num_channels = num_channels
+            self.bits_per_sample = bits_per_sample
+            self.encoding = encoding
+
+        def __iter__(self):
+            return iter((self.sample_rate, self.num_frames, self.num_channels, self.bits_per_sample, self.encoding))
+
+    common_mod.AudioMetaData = AudioMetaData
 except Exception:
     pass
 
